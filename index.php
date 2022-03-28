5145abfa6776e795da1b11b034a823b8d2b80e7b
Evgeniy585 <ev.zhuravlev95@mail.ru>
2022-03-28 15:54:19 +0300

    Создал формы для регистрации пользователя

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
-        <form class="form-signin" method="POST">
+        <form class="form-signin" method="post">
             <h2>Registration</h2>
             <input type="text" name="username" class="form-control" placeholder="Username" required>
             <input type="email" name="email" class="form-control" placeholder="Email" required>
