fc3535e301daecd88495e23b57fc221731e6e858
Evgeniy585 <ev.zhuravlev95@mail.ru>
2022-04-04 22:55:58 +0300

    Добавил сообщение о подключении или ошибке подключения

--- connect.php
+++ connect.php
@@ -1,5 +1,10 @@
 <?php
-
-$connection = mysqli_connect(localhost,root, " ");
-$select_db = mysqli_select_db($connection, practice);
+$connection = mysqli_connect("localhost", "root", "");
+$select_db = mysqli_select_db($connection, 'practice');
+if ($connection == false){
+    echo("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+}
+else {
+    echo("Соединение установлено успешно");
+}
 ?>
--- index.php
+++ index.php
@@ -1,22 +1,40 @@
-<<!doctype html>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href=" https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 </head>
 <body>
+
+<?php
+require ('connect.php');
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+}
+    $query = "INSERT INTO users (username, password, email) VALUES ('username', 'password', 'email')";
+    $result = mysqli_query($connection, $query);
+    if($result) {
+        $successfully = "Регистрация прошла успешно!";
+    } else {
+        $false = "Ошибка!";
+}
+?>
     <div class="container">
         <form class="form-signin" method="post">
             <h2>Registration</h2>
-            <input type="text" name="username" class="form-control" placeholder="Username" required>
-            <input type="email" name="email" class="form-control" placeholder="Email" required>
-            <input type="password" name="password" class="form-control" placeholder="Password" required>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+            <?php if(isset($successfully)){ ?><div class="alert alert-success" role="alert"> <?php echo $successfully; ?> </div><?php }?>
+            <?php if(isset($false)){ ?><div class="alert alert-danger" role="alert"> <?php echo $false; ?> </div><?php }?>
+                <input type="text" name="username" class="form-control" placeholder="Username" required>
+                <input type="email" name="email" class="form-control" placeholder="Email" required>
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
         </form>
     </div>
 </body>
