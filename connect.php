b77537fe42057786faca36219077d46bba755cf3
Evgeniy585 <ev.zhuravlev95@mail.ru>
2022-04-12 22:47:05 +0300

    Исправил строку с запросом к БД

--- connect.php
+++ connect.php
@@ -1,10 +1,5 @@
 <?php
     $connection = mysqli_connect('localhost', 'root', '');
-    $select_db = mysqli_select_db($connection, 'practice');
-    if($connection == false){
-        echo("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-    }
-    else {
-        echo("Соединение установлено успешно");
-}
+    $select_db = mysqli_select_db($connection, 'practice')
+       or die('Ошибка: подключение не выполнено.');
 ?>
