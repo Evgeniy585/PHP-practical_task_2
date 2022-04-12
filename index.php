146b4e3220c1af3284d1151fa4abfba08ac98982
Evgeniy585 <ev.zhuravlev95@mail.ru>
2022-04-12 12:43:22 +0300

    Исправил строку с запросом к БД

--- index.php
+++ index.php
@@ -12,29 +12,30 @@
 <body>
 
 <?php
-require ('connect.php');
+require('connect.php');
 if(isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $email = $_POST['email'];
-}
-    $query = "INSERT INTO users (username, password, email) VALUES ('username', 'password', 'email')";
+
+    $query = "INSERT INTO `users` (`username`, `password`, `email`) VALUES ('" . $username . "', '" . $password . "', '" . $email . "')";
     $result = mysqli_query($connection, $query);
-    if($result) {
+    if ($result) {
         $successfully = "Регистрация прошла успешно!";
     } else {
         $false = "Ошибка!";
+    }
 }
 ?>
     <div class="container">
-        <form class="form-signin" method="post">
+        <form class="form-signin" method="POST">
             <h2>Registration</h2>
             <?php if(isset($successfully)){ ?><div class="alert alert-success" role="alert"> <?php echo $successfully; ?> </div><?php }?>
             <?php if(isset($false)){ ?><div class="alert alert-danger" role="alert"> <?php echo $false; ?> </div><?php }?>
                 <input type="text" name="username" class="form-control" placeholder="Username" required>
                 <input type="email" name="email" class="form-control" placeholder="Email" required>
                 <input type="password" name="password" class="form-control" placeholder="Password" required>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
         </form>
     </div>
 </body>
