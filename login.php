7f8eec08a01bd614313b168596ced92e4ccfcc7a
Evgeniy585 <ev.zhuravlev95@mail.ru>
2022-04-20 21:32:41 +0300

    исправил запрос

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ require('connect.php');
 if(isset($_POST['username']) and isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $query = "SELECT * FROM users WHERE username='username' and password='password'";
+    $query = "SELECT * FROM users WHERE username='$username' and password='$password'";
     $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
     $count = mysqli_num_rows($result);
     if ($count == 1) {
